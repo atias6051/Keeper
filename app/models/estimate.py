63ada9ba28dd5a3942fe142ef9fc9462d22f58af
atias6051 <atias6051@gmail.com>
2023-03-22 17:59:12 -0400

    another ytu

--- app/models/estimate.py
+++ app/models/estimate.py
@@ -2,6 +2,7 @@ from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Estimate(db.Model):
     __tablename__ = 'estimates'
+
     if environment == "production":
         __table_args__ = {'schema': SCHEMA}
 
